9ae9068d54c9e4ceca6effd5e3f427ace6abd259
Foltinium <lixenses@gmail.com>
2026-04-13 13:24:45 +0300

    Реализация класса Mover

--- src/ru/gr094622/MainWindow.java
+++ src/ru/gr094622/MainWindow.java
@@ -1,12 +1,16 @@
 package ru.gr094622;
 
 import ru.gr094622.animating.Animator;
+import ru.gr094622.model.GeometryObject;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainWindow extends JFrame {
     private JPanel mainPanel;
     private Animator animator;
+    public List<GeometryObject> objects = new ArrayList<>();
     public MainWindow(){
 
     }
--- src/ru/gr094622/animating/Mover.java
+++ src/ru/gr094622/animating/Mover.java
@@ -1,5 +1,71 @@
 package ru.gr094622.animating;
 
-public class Mover implements Animatable{
+import ru.gr094622.model.GeometryObject;
 
+import java.awt.*;
+import java.util.List;
+
+public class Mover implements Animatable {
+    private List<GeometryObject> objects;
+    private Dimension size;
+    private boolean isRunning = false;
+    private Thread thread;
+
+    public Mover(List<GeometryObject> objects) {
+        this.objects = objects;
+    }
+
+    public void run() {
+        while (isRunning) {
+            moveFigures();
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    public void moveFigures() {
+        for (var obj : objects) {
+            int newX = obj.getX() + obj.getXSpeed();
+            int newY = obj.getY() + obj.getYSpeed();
+
+            if (newX < 0 || newX + obj.getSize().width > size.height) {
+                obj.inverseXSpeed();
+            } else {
+                obj.setX(newX);
+            }
+
+            if (newY < 0 || newY + obj.getSize().height > size.height) {
+                obj.inverseYSpeed();
+            } else {
+                obj.setY(newY);
+            }
+        }
+    }
+
+    @Override
+    public void start() {
+        if (isRunning) return;
+
+        isRunning = true;
+        thread = new Thread((Runnable) this);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        isRunning = false;
+    }
+
+    @Override
+    public Dimension getSize() {
+        return size;
+    }
+
+    @Override
+    public void setSize(Dimension size) {
+        this.size = size;
+    }
 }
